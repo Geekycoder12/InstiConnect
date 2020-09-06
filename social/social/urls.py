78b6928a79e2a4dc7f222a2fd22b7c93d4be614c
Geekycoder12 <supreet.singh.cse19@itbhu.ac.in>
2020-09-06 14:55:30 +0530

    Created a user app with login and logout

--- social/social/urls.py
+++ social/social/urls.py
@@ -18,5 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',include('signup.urls')),
+    path('users/',include('users.urls')),
 ]
